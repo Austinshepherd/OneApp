aac866e5067399e2df6178eb837bea3f640b53fb
Austinshepherd <austink.shepherd@gmail.com>
2020-05-19 02:47:01 -0700

    addInputNote onPress for button added.

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [inputNote, setInputNote] = useState('');
+
+  const NoteInputHandler = (enteredText) => {
+    setInputNote(enteredText);
+  };
+
+  const addNoteHandler = () => {
+    console.log(inputNote);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="new note"style={styles.inputBar}
+         onChangeText={NoteInputHandler}
+         value={inputNote}/>
+        <Button title="ADD" onPress={addNoteHandler} />
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+  const styles = StyleSheet.create({
+     screen: {
+       padding: 50
+     },
+
+    inputContainer: {
+      flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    inputBar: {
+      borderColor: 'grey',
+      borderWidth: 1,
+      borderRadius: 10,
+      padding: 10,
+      width: '80%'
+    }
+  
+  });
